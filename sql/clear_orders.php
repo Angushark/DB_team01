3173db25d1cae651e6a29b1fa0932384ba2556a2
Angus <f120909733@gmail.com>
2026-06-14 22:20:17 +0800

    Revert "fix type filter"
    This reverts commit 85cee922cbe908c7b091f9001c975395b602f3a4.

--- sql/clear_orders.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '../db_config.php';
-$conn->query("SET FOREIGN_KEY_CHECKS = 0");
-$conn->query("TRUNCATE TABLE `Contains`");
-$conn->query("TRUNCATE TABLE `Order`");
-$conn->query("SET FOREIGN_KEY_CHECKS = 1");
-echo "done";
